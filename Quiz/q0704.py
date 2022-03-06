b1af09fc0473d939b7cf7fe26f94488559c7ea90
Prakash Dhakal <sangita@DESKTOP>
2022-03-06 17:23:06 -0600

    Uploading solution of quiz 4.

--- Quiz/q0704.py
+++ Quiz/q0704.py
@@ -10,3 +10,47 @@ Example:
     """
 
     # answer
+#%%
+
+
+class Length:
+    @staticmethod
+    def cm_to_inches(cm):
+        return f'{cm} cm is {0.394*cm} inches.'
+    @staticmethod
+    def km_to_miles(km):
+        return f'{km} km is {0.621*km} miles.'
+
+print(Length.cm_to_inches(15))
+print(Length.km_to_miles(100))
+
+#%%
+
+
+class Weight:
+    @staticmethod
+    def kg_to_lb(kg):
+        return f'{kg} Kg is {2.204*kg} lb.'
+    @staticmethod
+    def gm_to_oz(gm):
+        return f'{gm} gm is {0.035*gm} OZ.'
+
+
+print(Weight.kg_to_lb(15))
+print(Weight.gm_to_oz(100))
+
+#%%
+
+
+class Time:
+    @staticmethod
+    def sec_to_h(sec):
+        return f'{sec} sec is {sec/3600} hour.'
+    @staticmethod
+    def milisec_to_sec(milisec):
+        return f'{milisec} milisec is {0.001*milisec} second.'
+
+print(Time.sec_to_h(3600))
+print(Time.milisec_to_sec(1000000))
+
+#%%
